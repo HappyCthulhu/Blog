2d50f8a4aec4d4c5380e0a71082c98328950a134
HappyCthulhu <valeriyg98@gmail.com>
2020-04-03 15:35:41 +0300

    'templates'

--- apps/blogapp/urls.py
+++ apps/blogapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_me, name="aboutme"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('post/', views.post, name="post"),
+    path('', views.index, name="index")
 ]
--- apps/blogapp/views.py
+++ apps/blogapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
-def about_me(request):
+def index(request):
+    return render(request, 'index.html', context={'lol': 'lol'})
+
+def about(request):
     return render(request, 'about.html', context={'lol': 'lol'})
+
+def contact(request):
+    return render(request, 'contact.html', context={'lol': 'lol'})
+
+def post(request):
+    return render(request, 'post.html', context={'lol': 'lol'})
