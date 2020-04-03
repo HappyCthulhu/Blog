e2f3ef421a7798abdd14322fd0894f49f6af1e54
hardls <fonati4ik@mail.ru>
2020-04-03 13:28:06 +0300

    second commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -16,7 +16,7 @@ import os, sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PROJECT_ROOT = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(PROJECT_ROOT, 'templates')
+            os.path.join(BASE_DIR, 'templates')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
--- blogproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
-from django.http import HttpResponse
-import blogproject.templates
-
-def main(request):
-    return render(request, 'index.html')
-
